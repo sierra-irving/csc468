a86615632f38ef257f2175f9d15f229276ed96c4
sierra-irving <90065301+sierra-irving@users.noreply.github.com>
2024-02-10 17:12:58 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -5,11 +5,10 @@ import geni.rspec.pg as rspec
 request = portal.context.makeRequestRSpec()
 # Create a XenVM
 node = request.XenVM("node")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
 node.routable_control_ip = "true"
 
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
-#node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y nginx"))
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
 node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
 
